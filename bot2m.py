41a961a7ec5baa022db95ac1e364397e7c5f84ae
Andrii <akashyren77@gmail.com>
2023-04-04 22:53:43 +0300

    Update bot2m.py

--- bot2m.py
+++ bot2m.py
@@ -6,6 +6,8 @@ class Field():
         self.value = value
     def __str__(self) -> str:
         return str(self.value)
+    def __repr__(self):
+        return f"{self.value}"
     
 
 class Name(Field):
